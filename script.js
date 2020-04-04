f658ea51a839ad5f3ad6dfa35580770d3a4a857c
Rich Capuy <rccapuy@gmail.com>
2020-04-04 19:37:59 -0400

    save icon

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 // THEN the current day is displayed at the top of the calendar
 
 var currentDay = moment();
+var momentTime = moment('LT');
+// console.log(currentDay);
+console.log(moment());
+console.log(moment('12 10').isBefore(moment()));
 // console.log(currentDay.format('dddd ' + 'MMM Do YYYY'));
-$("#currentDay").text(currentDay.format('dddd, ' + 'MMM Do YYYY'));
+$("#currentDay").text(currentDay.format('LLLL'));
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
@@ -14,6 +18,20 @@ var hours =
 var scheduler = $(".container");
 
 $.each(hours, function(i, time){
-	scheduler.append('<div class="time-block"><div class="row"><div class="hour">' + hours[i] + '</div><textarea class="description" placeholder="Description"></textarea><button class="saveBtn">Button</button></div></div>');
+	scheduler.append('<div class="time-block"><div class="row"><div class="hour">' + hours[i] + '</div><textarea class="description" placeholder="Description"></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div></div>');
 	console.log(time);
-})
+});
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+var description = $
+
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+
